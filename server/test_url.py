3616fa477b9f85f880217581c249512bfad7eb78
gon2gon2 <ark5354@gmail.com>
2025-10-04 16:49:51 +0900

    feat: URL class supports  scheme

---
+++ server/test_url.py
@@ -0,0 +1,4 @@
+from url import URL
+
+def test_url_support_http_only():
+    url = URL("http://asdasd.com")
---
+++ server/url.py
@@ -0,0 +1,5 @@
+class URL:
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
+        
